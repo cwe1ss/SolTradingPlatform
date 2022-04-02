200b0000fbd8fe83a026f2aa1221afd0f2b49962
Christian Weiss <christian@chwe.at>
2022-04-02 10:13:19 +0200

    Aufgabe 3 (Retries / Round-Robin)

--- MeiShop/Program.cs
+++ MeiShop/Program.cs
@@ -1,3 +1,5 @@
+using MeiShop.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Das RoundRobinService muss Request-übergreifend die einzelnen Adressen durchlaufen
+// und wird daher als Singleton-Service registriert.
+builder.Services.AddSingleton<RoundRobinService>();
+
+// Alternative Retry-Logik im CashDeskController könnte auch über die 3rd-Party Library "Polly" durchgeführt werden,
+// mit der man zentral das Retry-Behavior über Policies steuern kann.
+//builder.Services.AddHttpClient("CreditCards", client =>
+//{
+//    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+//}).AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.WaitAndRetryAsync(new[]
+//{
+//    TimeSpan.FromSeconds(1),
+//    TimeSpan.FromSeconds(5),
+//    TimeSpan.FromSeconds(10),
+//}));
 
 var app = builder.Build();
 
