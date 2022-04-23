fdd01986b8e9598cb9070714ccda0bb242c16dfc
Johann Grabner <jgrabner@business-tuning.at>
2022-04-23 08:00:33 +0200

    Discovery with Consul by HashiCorp

---
+++ IEGEasyCreditCardService/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IEGEasyCreditCardService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet("")]
+        [HttpHead("")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+
+    }
+}
--- MeiShop/Controllers/CashDeskController.cs
+++ MeiShop/Controllers/CashDeskController.cs
@@ -1,4 +1,5 @@
-﻿using MeiShop.Models;
+﻿using Consul;
+using MeiShop.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
@@ -29,8 +30,8 @@ namespace MeiShop.Controllers
         {
             _logger.LogInformation($"TransactionInfo Creditcard: {basket.CustomerCreditCardnumber} Product:{basket.Product} Amount: {basket.AmountInEuro}");
 
-            var creditcardServiceBaseAddress = Configuration["CreditcardServiceBaseAddress"];
-
+            //var creditcardServiceBaseAddress = Configuration["CreditcardServiceBaseAddress"];
+            var creditcardServiceBaseAddress = GetCreditCardTransactionsURIFromConsul().AbsolutePath;
             //Mapping
             CreditcardTransaction creditCardTransaction = new CreditcardTransaction()
             {
@@ -48,6 +49,35 @@ namespace MeiShop.Controllers
 
 
             return CreatedAtAction("Get", new { id = System.Guid.NewGuid() }, creditCardTransaction);
+        }
+
+     
+
+        private Uri GetCreditCardTransactionsURIFromConsul()
+        {
+
+            List<Uri> _serverUrls = new List<Uri>();
+            var consuleClient = new ConsulClient(c => c.Address = new Uri("http://127.0.0.1:8500"));
+            var services = consuleClient.Agent.Services().Result.Response;
+            foreach (var service in services)
+            {
+                var isCreditCardApi = service.Value.Tags.Any(t => t == "CreditCard");
+                if (isCreditCardApi)
+                {
+                    try
+                    {
+                        var serviceUri = new Uri($"{service.Value.Address}:{service.Value.Port}");
+                        _serverUrls.Add(serviceUri);
+                    }
+                    catch (Exception)
+                    {
+
+                        ;
+                    }
+
+                }
+            }
+            return _serverUrls.FirstOrDefault();
         }
     }
 }
