9e45f36460d4c416a77c748f06ee3f4fb1c5a2b2
Daniel Lieb <s50736@edu.campus02.at>
2022-05-07 17:12:35 +0200

    improved FormDraftService; added ConfigService

---
+++ ConfigService/Controllers/LoadBalancer.cs
@@ -0,0 +1,38 @@
+﻿using ConfigService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConfigService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoadBalancer : ControllerBase
+    {
+        public static IList<Deployment> DeploymentList = new List<Deployment>()
+        {
+            new Deployment()
+            {
+                Id = 1,
+                ServiceType = "FormDraftService",
+                URL = "https://formdraftservice20220507143259.azurewebsites.net"
+            },
+            new Deployment()
+            {
+                Id = 2,
+                ServiceType = "FormDraftService",
+                URL = "https://formdraftservice20220507145531.azurewebsites.net"
+            }
+        };
+
+        // GET: api/<LoadBalancer>
+        [HttpGet("{ServiceType}")]
+        public string Get(string ServiceType)
+        {
+            IList<Deployment> URL_list = DeploymentList.Where(element => element.ServiceType == ServiceType).ToList();
+            var random = new Random();
+            int index = random.Next(URL_list.Count);
+            return URL_list[index].URL;
+        }
+    }
+}
---
+++ ConfigService/Models/Deployment.cs
@@ -0,0 +1,9 @@
+﻿namespace ConfigService.Models
+{
+    public class Deployment
+    {
+        public int Id { get; set; }
+        public string ServiceType  { get; set; } //z.B. Draft-Service
+        public string URL { get; set; }
+    }
+}
