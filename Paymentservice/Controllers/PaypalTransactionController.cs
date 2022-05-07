e969a9ed304531b3cb4fadbe8ca5d8867b9891ba
LiskofIt <akofa.djomatin@gmail.com>
2022-05-07 13:30:28 +0200

    Add payementService, QuestionAnswersService

---
+++ Paymentservice/Controllers/PaypalTransactionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paymentservice.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Paymentservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaypalPaymentTransactionController : ControllerBase
+    {
+        private readonly ILogger<PaypalPaymentTransactionController> _logger;
+        public PaypalPaymentTransactionController(ILogger<PaypalPaymentTransactionController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: api/<PaypalPaymentTransactionController>
+        // [HttpGet]
+        [HttpGet(Name = "GetPaypalPaymentTransaction")]
+        public string Get(int id)
+        {
+            _logger.LogInformation("Paypal payment");
+             return "value" + id;
+
+        }
+
+        // POST api/<PaypalPaymentTransactionController>
+        [HttpPost]
+      
+        public IActionResult Post([FromBody] PaypalPaymentTransaction paypalPaymentTransaction)
+        {
+            _logger.LogInformation($"TransactionInfo Username: {paypalPaymentTransaction.PayPalUsername} Amount:{paypalPaymentTransaction.Amount} Password: { paypalPaymentTransaction.PayPalPasswort}");
+
+            return CreatedAtAction("Get", new { id = System.Guid.NewGuid() });
+        }
+
+     
+
+        }
+    }
+
---
+++ QuestionnaireAnswersService/Models/FormDraft.cs
@@ -0,0 +1,13 @@
+﻿using QuestionnaireAnswersService;
+
+namespace FormDraftService.Models
+{
+    public class FormDraft
+    {
+        public String FormId { get; set; }
+        public String Name { get; set; }
+        public String Status { get; set; }
+        public String Description { get; set; }
+        public IEnumerable<Question> Questions { get; set; }
+    }
+}
